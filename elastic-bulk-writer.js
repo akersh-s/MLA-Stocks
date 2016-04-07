e6b4c62b24c1dea6f69e5a0f3ad3a07adb1e1036
Akersh Srivastava <akersh.srivastava@accenture.com>
2016-04-06 22:46:42 -0700

    last minute change

--- elastic-bulk-writer.js
+++ elastic-bulk-writer.js
@@ -5,24 +5,39 @@ var sentiment = require('sentiment');
 var stream = fs.createReadStream('data/backup/stocktwits_messages_jan_2016.json', {
     encoding: 'utf8'
 });
-
+var i = 0;
 
 var parser = JSONStream.parse();
 
 var client = new elasticsearch.Client({
-    host: 'localhost:9200'
+    host: 'localhost:9200',
+    log: 'info'
 });
 
 stream.pipe(parser);
 
 parser.on('data', function(obj) {
+    i++;
+    console.log(i);
+    if (i == 1000) {
+        stream.pause();
+        console.log('paused');
+        setTimeout(function(){
+            console.log('resumed');
+            i = 0;
+            stream.resume();
+        },1000);
+    }
+    var arr = [];
     console.log("read");
     var customSentiment = sentiment(obj.body);
     var newObj = {
         obj: obj,
         customSentiment: customSentiment
     }
-    processLine(newObj);
+    arr.push({ "index" : { "_index" : "jan_twits_newest", "_type" : "block", "_id" : obj.id } })
+    arr.push(newObj);
+    processLine(arr);
 });
 
 
@@ -32,10 +47,8 @@ function processLine(line) { // here's where we do something with a line
     console.log("processing");
 
 
-    client.create({
-        requestTimeout: 1000000000000,
-        index: 'test123',
-        type: 'block',
+    client.bulk({
+        requestTimeout: 300000000,
         body: line
     }, function(error, response) {
         if (error) {
@@ -43,6 +56,6 @@ function processLine(line) { // here's where we do something with a line
         }
     });
 
-    console.log("record created");
+    console.log("record created " + line[1].obj.id);
 
 }
